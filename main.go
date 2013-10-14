00fe4e99e49f430663b68ee1cf92b870475913a7
Thiago Pradi <thiago.pradi@gmail.com>
2013-10-14 01:06:33 -0300

    Adding examples for reading the program from the file

--- main.go
+++ main.go
@@ -2,14 +2,22 @@ package main;
 
 import (
   "flag"
+  "io/ioutil"
   "github.com/tchandy/brainfuck_compiler_go/compiler"
 )
 
 func main() {
   var code string 
+  var filepath string
 
   flag.StringVar(&code, "c", "", "The code to run")
+  flag.StringVar(&filepath, "f", "", "The File to run the code")
   flag.Parse()
 
-  compiler.RunProgram(code)
+  if code != "" {
+    compiler.RunProgram(code)
+  } else if filepath != "" {
+    content, _ := ioutil.ReadFile(filepath)
+    compiler.RunProgram(string(content))
+  }
 }
